d2744c3618b8070d3caeb4318ab9da9b4b542de3
Umar Ahmed <umar.ahmed@mail.utoronto.ca>
2020-08-14 13:28:21 -0400

    feat: render heightmap and run prettier

--- main.js
+++ main.js
@@ -1,15 +1,40 @@
-window.addEventListener('DOMContentLoaded', main)
+window.addEventListener("DOMContentLoaded", main);
+
+const WIDTH = 256;
+const HEIGHT = 256;
+
+function render(s, t) {
+  return [s, t, 0.5, 1.0];
+}
 
 function main() {
-    console.log('start')
+  console.log("start");
 
-    const canvas = document.getElementById('canvas')
-    const ctx = canvas.getContext('2d')
+  const canvas = document.getElementById("canvas");
+  const ctx = canvas.getContext("2d");
 
-    ctx.moveTo(0,0)
-    ctx.lineTo(255, 255)
-    ctx.strokeStyle = 'red'
-    ctx.stroke()
+  const imageData = ctx.getImageData(0, 0, WIDTH, HEIGHT);
+  const heightmap = imageData.data;
 
-    console.log('done')
+  // Update heightmap
+  for (let i = 0; i < heightmap.length; i += 4) {
+    const j = i / 4;
+
+    const x = j % WIDTH;
+    const y = Math.floor(j / WIDTH);
+
+    const s = x / WIDTH;
+    const t = y / HEIGHT;
+
+    const [r, g, b, a] = render(s, t);
+
+    heightmap[i] = r * 255;
+    heightmap[i + 1] = g * 255;
+    heightmap[i + 2] = b * 255;
+    heightmap[i + 3] = a * 255;
+  }
+
+  ctx.putImageData(imageData, 0, 0);
+
+  console.log("done");
 }
