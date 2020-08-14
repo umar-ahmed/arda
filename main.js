83856dd040780b39aab1a5b19affbd50e08b2f8b
Umar Ahmed <umar.ahmed@mail.utoronto.ca>
2020-08-14 13:01:55 -0400

    wip: render canvas

--- main.js
+++ main.js
@@ -1,5 +1,15 @@
 window.addEventListener('DOMContentLoaded', main)
 
 function main() {
-    console.log('main')
+    console.log('start')
+
+    const canvas = document.getElementById('canvas')
+    const ctx = canvas.getContext('2d')
+
+    ctx.moveTo(0,0)
+    ctx.lineTo(255, 255)
+    ctx.strokeStyle = 'red'
+    ctx.stroke()
+
+    console.log('done')
 }
